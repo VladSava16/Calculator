35547808a684ca4e57241d824b65d350b2a70a11
Vlad Sava <savavlad82@gmail.com>
2022-03-26 14:46:32 +0200

    Fix delete button
    Fixed the delete button so as to work properly with negative numbers.
    Will change the input to string and later convert it to number in order
    to make it work with decimal numbers.

--- script.js
+++ script.js
@@ -30,14 +30,32 @@ function Clear(){
     answer.textContent = "";
 }
 
+function deleteLastDigit(currentNo){
+    if(currentNo == 1){
+        if(no1 > 0)
+            no1 = Math.floor(no1 / 10);
+        else
+            no1 = Math.ceil(no1 / 10);
+    }
+    else{
+        if(no2 >0)
+            no2 = Math.floor(no2 / 10);
+        else
+            no2 = Math.ceil(no2 / 10);
+    }
+    currentExpression.textContent = no1 + op + no2;
+}
+
 let no1 = null;
 let no2 = null;
 let op = null;
 let ans = null;
+let currentNo = 1;
 
 const currentExpression = document.querySelector(".current-expression");
 const answer = document.querySelector(".answer");
 const clearButton = document.querySelector(".clear");
+const deleteButton = document.querySelector(".delete");
 
 const numberButtons = document.querySelector(".numbers");
 numberButtons.childNodes.forEach(btn => btn.addEventListener("click", (e) =>{
@@ -56,10 +74,12 @@ operatorButtons.childNodes.forEach(operator => operator.addEventListener("click"
         no1 = partAns;
         no2 = null;
         op = e.target.textContent;
+        currentNo = 2;
     }
     else{
         op = e.target.textContent;
         console.log(op);
+        currentNo = 2;
     }
     currentExpression.textContent = no1 + op;
 }));
@@ -72,7 +92,9 @@ equalButton.addEventListener("click", (e) =>{
         answer.textContent = operate(no1, no2, op);
         no1 = +answer.textContent;
         op = no2 = null;
+        currentNo = 1;
     }
 });
 
-clearButton.addEventListener("click", () => Clear());
+clearButton.addEventListener("click", () => Clear()); // handles the clear onclick event
+deleteButton.addEventListener("click", () => deleteLastDigit(currentNo));
