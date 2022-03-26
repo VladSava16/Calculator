b2d677ca67e13881a94bc394531a0c0f94f80aeb
Vlad Sava <savavlad82@gmail.com>
2022-03-26 11:37:10 +0200

    Add operate function and 2nd number input
    Added an operate function that handles the evaluation of the mathematical
    expression.
    Also added input for the 2nd number depending on the value of the operator.
    If the operate is "null" then the user still wants to input the first number.
    Otherwise, they want to input the second number.

--- script.js
+++ script.js
@@ -13,13 +13,39 @@ function divide(a, b){
     return a / b;
 }
 
+function operate(a, b, op){
+    if(op == "+")
+        return add(a, b);
+    else if(op == "-")
+        return subtract(a, b);
+    else if(op == "*")
+        return multiply(a, b);
+    else
+        return divide(a, b);
+}
+
 let no1 = null;
 let no2 = null;
 let op = null;
 
-let numberButtons = document.querySelector(".numbers");
+const numberButtons = document.querySelector(".numbers");
 numberButtons.childNodes.forEach(btn => btn.addEventListener("click", (e) =>{
     if(op === null)
         no1 = no1 * 10 + +e.target.textContent;
-    console.log(no1);
+    else
+        no2 = no2 * 10 + +e.target.textContent;
+    console.log(no1, no2);
+}));
+
+const operatorButtons = document.querySelector(".operators");
+operatorButtons.childNodes.forEach(operator => operator.addEventListener("click", (e) => {
+    if (e.target.classList.value == "plus")
+        op = "+";
+    else if(e.target.classList.value == "minus")
+        op = "-";
+    else if(e.target.classList.value == "multiply")
+        op = "*";
+    else
+        op = "/";
+    console.log(op);
 }));
